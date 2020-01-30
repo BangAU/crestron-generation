bf87319b5af8f534492ae9b7487d5fa180bb3a14
francisbang <59549139+francisbang@users.noreply.github.com>
2020-01-31 09:02:38 +1100

    initial commit desktop design

---
+++ body.php
@@ -0,0 +1,216 @@
+<section class="top-page">
+  <header id="header">
+    <div class="menu">
+      <div class="container">
+        <nav id="nav">
+          <input type="checkbox" id="show-mobile">
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul>
+              <li><a href="#it-works">KNOW IT WORKS</a></li>
+              <li><a href="#collaborate">COLLABORATE WITH CONFIDENCE</a></li>
+              <li><a href="#performance">OUR PERFORMANCE</a></li>
+              <li><a href="#believe">BELIEVE IN CRESTRON</a></li>
+              <li><a href="#work">WORK WITH US</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="logo-container">
+      <div class="container">
+        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-crestron.png" width="177" alt="Crestron Partners"></a>
+        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/generation-logo.svg"  alt="Generation"></a>
+      </div>
+    </div>
+  </header>
+  <div class="banner">
+    <div class="banner-image">
+      <div class="container">
+        <div class="banner-holder">
+          <div class="bg-back">
+          </div>
+          <div class="bg-front">
+            <img src="images/banner-front.png" width="1028" height="310" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="banner-contents">
+      <div class="container">
+        <div class="banner-text">
+          <h1>
+          Generation-e and Crestron positively transform modern workspaces.
+          </h1>
+          <p>What should a modern workspace feel like? Flexible and inspiring rooms, high quality audio and visual meeting and conferencing technology, human spatial design, and work-life balance all coming together for more wonderful and productive experiences.</p>
+          <div class="btn-holder">
+            <a href="#" class="btn">GET IN TOUCH</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="it-works" id="it-works">
+  <div class="container">
+    <div class="it-works-container">
+      <div class="it-works-left-panel">
+        <div class="it-works-content">
+          <h2>Just know it works</h2>
+          <p>Together, Generation-e and Crestron bring you expertly engineered and supported unified communications and audio-visual solutions that will positively transform your team’s workspace experience. </p>
+          <p>Backed by our proven methodology, we deliver a tailored integration of audio visual, digital, networking, and communications technology that lets your team simply and securely meet, collaborate, and share.</p>
+          <div class="it-works-logo-content">
+             <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/crestron-middle-logo.png" width="177"  alt="Crestron Partners"></a>
+             <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/itworks-logo.png"  alt="Ten Four"></a>
+          </div>
+        </div>
+      </div>
+      <div class="it-works-right-panel">
+        <img src="images/it-works-top.svg" alt="it-works-top" class="top-img">
+        <img src="images/it-works-main.png" alt="it-works-main" class="main-img">
+      </div>
+    </div>  
+  </div>
+</section>
+
+<section class="collaborate" id="collaborate" >
+  <div class="container">
+    <div class="collaborate-wrap">
+      <h2>Collaborate with confidence </h2>
+      <div class="collaborate-container">
+        <div class="collaborate-left">
+          <h3>An amazing workplace for modern productivity.</h3>
+        </div>
+        <div class="collaborate-right">
+          <p>Organisations across Australia are rapidly improving workspace experiences to remain productive, competitive, and attractive to employees.  </p>
+            <p>At Generation-e, we enable modern productivity across your organisation through professional consulting, change management, and support services in which we are proven industry leaders.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <img src="images/middle-red-line.svg" style="float:right;" class="middle-red-line">
+</section>
+
+<section class="performance" id="performance" >
+  <div class="container ">
+    <div class="performance-container">
+        <h2>We’re passionate about excellence</h2>
+      <div class="performance-contents ">
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/passion-first.svg">
+            </div>
+            <h5>Driving innovation  </h5>
+            <p>Generation-e pioneers emerging workspace areas like Smart Spaces (using IoT tech), Pure IP and Pure Cloud meeting rooms to enable faster, cable free deployments across multiple sites.
+            </p>
+
+          </div>
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/passion-middle.svg">
+            </div>
+            <h5>Trusted by leaders </h5>
+            <p> Leading Australian brands choose Generation-e for workspace transformation, including Bupa, Virgin, icare NSW, Audi, Dulux, University of Melbourne, and Government of Victoria.
+            </p>
+
+          </div>
+          <div class="side">
+            <div class="img-wrap">
+              <img src="images/passion-last.svg">
+            </div>
+            <h5>Fully certified framework  </h5>
+            <p> Generation-e developed a ‘Modern Meeting Success Framework’ with Deloitte. We’re also the only APAC member of Microsoft Teams’ FastTrack program for modern meeting support and training.
+            </p>
+          </div>
+      </div>
+    </div> 
+  </div>
+</section>
+
+<section class="believe" id="believe">
+    <div class="container">
+      <div class="believe-container">
+        <div class="believe-left">
+          <h2>Believe in Crestron</h2>
+          <p>For more than 45 years, Crestron has helped businesses like yours with beautifully engineered automation, audio visual and unified communications equipment for modern workplaces.
+          </p> 
+          <p> Crestron technology simply works wonders, producing inspired collaboration you can believe in.</p>
+          <a class="btn">Find out more</a>
+        </div>
+        <div class="believe-right">
+          <div class="video-holder">
+            <iframe src="https://www.youtube.com/embed/amuH0Gs5ZhjhhjY" frameborder="0" style="    width: 663px;
+        height: 404px;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>  
+</section>
+
+<section class="work" id="work">
+  <div class="container">
+    <div class="work-container">
+
+      <img src="images/middle-red-line.svg"  class="red-left-line-works">
+      
+      <div class="work-form">
+        <h2>Work With Us</h2>
+        <p>Work with Generation-e and Crestron to start empowering your people with more satisfying and productive world-class collaboration experiences. 
+          Use the form to get in touch and book a  <strong>free on-site demonstration</strong> at your office!</p>
+        <form action="form-submit.php" id="work-form" class="work-form">
+          <div class="form-group">
+            <div class="col">
+              <input type="text" name="firstname" placeholder="First Name" data-rule-required="true" data-msg-required="Please enter your first name">
+            </div>
+            <div class="col">
+              <input type="text" name="lastname" placeholder="Last Name" data-rule-required="true" data-msg-required="Please enter your last name">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col">
+              <input type="text" name="organization" placeholder="Organization">
+            </div>
+            <div class="col">
+              <input type="email" name="email" placeholder="Email" data-rule-required="true" data-msg-required="Please enter your email">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col">
+              <input type="tel" name="phone" placeholder="Phone" data-rule-minlength="10" data-rule-maxlength="10" data-rule-required="true" data-msg-required="Please enter your phone number">
+            </div>
+            <div class="col">
+              <select class="role" name="role">
+                <option selected value="Not specified">Role in Company</option>
+                <option selected value="CEO">CEO</option>
+                <option selected value="CTO">CTO</option>
+                <option selected value="Others">Others</option>
+              </select>
+              <!-- <input placeholder="Role in Company" type="text" class="role" list="roles" name="role">
+              <datalist id="roles">
+                <option value="CEO">
+                <option value="CTO">
+                <option value="Marketing Department">
+                <option value="Others">
+              </datalist> -->
+            </div>
+          </div>
+          <div class="form-group">
+            <textarea name="comments" placeholder="Comments"></textarea>
+          </div>
+          <div class="form-group btn-holder">
+            <input type="submit" class="btn btn-submit" value="submit">
+          </div>
+        </form>
+        <div class="message-success">
+          <p>Thank you for registering your interest. A member of our team will be in contact with you shortly.</p>
+        </div> <!-- end .message-success --> 
+      </div>
+
+      <div class="left-straight-block">
+      </div>
+
+    </div>
+  </div>
+</section>
---
+++ footer.php
@@ -0,0 +1,19 @@
+
+    <footer id="footer">
+        <div class="footer-container">
+            <div class="container">
+                <div class="footer-contents">
+                    <div class="left-footer">
+                        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-crestron.png" width="177" style="margin-top:30px;" alt="Crestron Partners"></a>
+                        <a href="https://www.crestron-partners.com.au/" target="_blank"><img src="images/logo-e-generation.png" alt="E-generation"></a>
+                    </div>
+                    <div class="right-footer">
+                        <span class="copyright">&copy;  2020 Crestron Electronics, Inc.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/app.js"></script>
+</body>
+</html>
